eddcfc192c907bed4ac1ac10f888607cc51df0d7
hygnic <hygnic@outlook.com>
2020-07-28 01:08:05 +0800

    study cv2

--- DCcore/dcutility.py
+++ DCcore/dcutility.py
@@ -13,8 +13,7 @@ from time import sleep
 import random
 import os
 import cv2
-import logging as lg
-
+import logging as lg  # TODO 设置其他颜色的日志输出未果
 from conf import pathfile
 from conf.DClocation import Location
 
@@ -22,7 +21,7 @@ pf = pathfile.Path()
 lt = Location()
 # 日志信息设置
 lg.basicConfig(
-	format="%(levelname)s:%(message)s>>%(asctime)s>>%(funcName)s",
+	format="%(asctime)s >> %(funcName)s %(levelname)s: %(message)s",
 	datefmt="%d-%m-%Y %H:%M:%S", level=lg.DEBUG)
 
 
@@ -114,37 +113,54 @@ def adb_back(a=0.1, b=0.3):
 	os.system(back_code)
 
 
+# class NewList(list):
+# 	def __init__(self,num):
+# 		list.__init__([])
+# 		self.num = num
+# 		slen = len(self)
+# 		if slen>num:
+# 			self = self[2:]
+#
+# 	def get_len(self):
+# 		pass
+
+
 # 不用类 麻烦！
 class MatchTool(object):
 	pf = pathfile.Path()
 	
 	def __init__(self, method):
 		"""
-		
-		:param method: 图像匹配方法 cv2.TM_CCOEFF_NORMED
+		:param method:
+			method usualy :cv2.TM_CCOEFF_NORMED
 		"""
 		self.method = method
-		# cv2 读取的屏幕截图组
-		self.screenshots = []
+		# cv2.imread 读取的屏幕截图组
+		self.screenshots = [1,2,3,4]
 		
-		print self.pf.mian_flags
-	
-		
-	def get_screenshots(self):
-		if len(self.screenshots)>4:
-			self.screenshots = self.screenshots[-4:]
+	# use this function to call self.screenshots
+	def return_screenshots(self):
+		# limit the number of items within 4
+		if len(self.screenshots)>3: # TODO 填3才会变成4，不管了，反正只有最后两张图片
+			self.screenshots = self.screenshots[-3:]
 		return self.screenshots
 	
-	def get_screen(self):
+	def screencapture(self):
+		# 使用adb内置截屏功能
+		# 返回最新截屏的cv2读取数据
 		cmd_get = "adb shell screencap -p /sdcard/screen_img.png"
 		cmd_send = "adb pull sdcard/screen_img.png G:/MoveOn/boomboost/image"
 		os.system(cmd_get)
 		os.system(cmd_send)
-		# 记录当前屏幕截图
+		# 记录当前屏幕截图（cv2读取数据）
 		screen_image = cv2.imread("../image/screen_img.png", 0)
-		self.screenshots.append()
+		# 将截图（cv2读取数据）加入self.screenshots列表中
+		screens = self.return_screenshots()
+		screens.append(screen_image)
+		return screen_image
 	
 	def back_to_main_menu(self):
+		# return DC main menu(ongoing)
 		sleeptime(1, 1.5)
 		cv2value1, cv2real_value = image_match(self.pf.mian_flags, 0.8)
 		if 1 not in cv2value1:
@@ -152,20 +168,26 @@ class MatchTool(object):
 			adb_back()
 			self.back_to_main_menu()
 	
-	def image_match(self, temp, match_value):
+	def image_match(self, temp, threshold):
 		"""
-		temp(String\Unicode\List): 匹配标志(模板)图片地址
-		match_value(Int\Float): 阈值 Threshold，cv2匹配结果大于阈值返回1
+		使用模板文件（temp）与当前屏幕进行实时匹配
+		temp(String\Unicode\List): 匹配标志(模板)图片地址或者地址列表
+		threshold(Int\Float): 阈值 Threshold，cv2匹配结果大于阈值返回1
 		screen(String): default value
 		:return: 返回1或者0，以及一个图像识别匹配最大值
 		"""
-		# 发送图片
-		cmd_get = "adb shell screencap -p /sdcard/screen_img.png"
-		cmd_send = "adb pull sdcard/screen_img.png G:/MoveOn/boomboost/image"
-		os.system(cmd_get)
-		os.system(cmd_send)
-		# 记录当前屏幕截图
-		screen_image = cv2.imread("../image/screen_img.png", 0)
+		# get screencapture
+		# 1.old method
+		# cmd_get = "adb shell screencap -p /sdcard/screen_img.png"
+		# cmd_send = "adb pull sdcard/screen_img.png G:/MoveOn/boomboost/image"
+		# os.system(cmd_get)
+		# os.system(cmd_send)
+		# # 记录当前屏幕截图
+		# screen_image = cv2.imread("../image/screen_img.png", 0)
+		# 2.new method
+		# 也可以使用 self.screen_image 来达到各个方法都能方面屏幕截图，
+			# 但是可以用事先写好的 self.return_screenshots 获取最近的4张屏幕截图列表
+		screen_image = self.screencapture()
 		
 		if isinstance(temp, list):
 			cv2images = []
@@ -177,19 +199,97 @@ class MatchTool(object):
 				cv2match_result.append(result.max())
 			# 列表推导式 大于阈值的为1，小于阈值的为0
 			judgement_result = [
-				1 if i > match_value else 0 for i in cv2match_result]
+				1 if i > threshold else 0 for i in cv2match_result]
 			# return list just like that: [1,0,1]
 			return judgement_result, cv2match_result  # [1, 0, 1],[0.934, 0.349, 0.987]
 		
 		if isinstance(temp, str) or isinstance(temp, unicode):
 			temp_image = cv2.imread(temp, 0)  # 只读取灰度图
-			result = cv2.matchTemplate(screen_image, temp_image, self.method)
-			result = result.max()
-			if result > match_value:
+			result1 = cv2.matchTemplate(screen_image, temp_image, self.method)
+			result = result1.max()
+			if result > threshold:
+				# 显示图像
+				self.find_rectangle(temp_image, result1)
 				return 1, result
 			else:
+				lg.debug("1104 not matched")
+				# 显示图像
+				self.find_rectangle(temp_image, result1)
 				return 0, result
+			
+	# 给匹配上的图形添加一个图框，同时显示一个内缩的图框，返回内缩图框中的一个点
+	def find_rectangle(self,cv2temp,result):
+		""""""
+		min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+		# 获取模板文件的高和宽，h, w
+		h, w = cv2temp.shape[:2]
+		max_lefttop = max_loc
+		max_rightbottom = (max_lefttop[0] + w, max_lefttop[1] + h)
+		
+		def get_randxy2(x, y):  # (570, 650),(240, 310)
+			"""产生一个在x,y二维区域内的随机位置,x,y为两个元素的列表，变量范围"""
+			xc = random.randint(x[0], x[1])
+			yc = random.randint(y[0], y[1])
+			print "x,y:", xc, yc
+			return xc, yc
+		
+		zoom_value = 0.25 # 缩放减去的范围，相当于减少X%的高和宽
+		# (x1,y1)是左上角的点，(x2,y2)是右下角的点，矩形内缩的话右上角的点值增加，右下角的点值减小
+		x1 = max_lefttop[0]
+		x2 = max_rightbottom[0]
+		y1 = max_lefttop[1]
+		y2 = max_rightbottom[1]
+		xlength = x2-x1
+		ylength = y2-y1
+		reduce_xlength = int(xlength*zoom_value//2)
+		reduce_ylength = int(ylength*zoom_value//2)
+		
+		newx1 = x1+reduce_xlength
+		newy1 = y1+reduce_ylength
+		newx2 = x2-reduce_xlength
+		newy2 = y2-reduce_ylength
+		new_lefttop = (newx1,newy1)
+		new_rightbottom = (newx2,newy2)
+		# 随机获取缩小图框中的一个点
+		random_point = get_randxy2((newx1, newx2),(newy1,newy2))
+		lg.debug("random_point:{}".format(random_point))
+		
+		lg.debug("xlength:{},ylength:{}".format(xlength, ylength))
+		lg.debug(max_lefttop)
+		lg.debug(max_rightbottom)
+		
+		image1 = self.return_screenshots()[-1]
+		cv2.rectangle(image1, max_lefttop, max_rightbottom, [0, 0, 0],3)
+		cv2.rectangle(image1, new_lefttop, new_rightbottom, [0, 0, 0],2)
+		lg.debug("show screen detected")
+		# cv2.resizeWindow("DC", 90, 100) # 改变大小，不是缩放
+		# 显示图像以及识别的图框
+		def show_iamge():
+			cv2.imshow("DC", self.screen_image)
+			# cv2.destroyAllWindows()
+			# 使opencv图像显示界面长期存在
+			cv2.waitKey(1)
+		
+		show_iamge()
 
-
+		
+		
 if __name__ == '__main__':
 	os.chdir("../adb")
+	# os.system("adb connect 127.0.0.1:21503")
+	mt = MatchTool(cv2.TM_CCOEFF_NORMED)
+	# mt.screencapture()
+	# mt.screencapture()
+	# mt.screencapture()
+	# ss= mt.screencapture()
+	# th, tw = ss.shape[:2]
+	# print th,tw
+	# print "len:",len(mt.screenshots)
+	# print mt.screenshots
+	mt.image_match(ur"G:\MoveOn\boomboost\image\battle.png",0.8)
+	
+	humanbeing_click(lt.nightwordX, lt.nightwordY)
+	sleeptime(2,3)
+	mt.image_match(ur"G:\MoveOn\boomboost\image\spp1.png", 0.8)
+	humanbeing_click(lt.menu_springX, lt.menu_springY)
+	
