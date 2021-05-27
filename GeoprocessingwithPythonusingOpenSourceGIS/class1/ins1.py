378c3b9fe186dc1947e076e6174878f8b2da5a63
hygnic <hygnic@outlook.com>
2021-05-27 16:52:04 +0800

    uplode GeoprocessingwithPythonusingOpenSourceGIS

---
+++ GeoprocessingwithPythonusingOpenSourceGIS/class1/ins1.py
@@ -0,0 +1,43 @@
+# -*- coding:utf-8 -*-
+# -------------------------------------------
+# Name:              ins1
+# Author:            Hygnic
+# Created on:        2021/5/27 15:45
+# Version:           
+# Reference:         
+"""
+Description:         
+Usage:               
+"""
+# -------------------------------------------
+import sys
+try:
+    from osgeo import ogr
+except:
+    import ogr
+
+
+driver = ogr.GetDriverByName('ESRI Shapefile')
+fn = "../ospy_data1/sites.shp"
+
+dataSource = driver.Open(fn, 0)
+
+if dataSource is None:
+    print('Could not open ' + fn)
+    sys.exit(1) #exit with an error code
+    
+layer = dataSource.GetLayer()
+print("Features Count:{}".format(layer.GetFeatureCount()))
+print("Features Extent:{}".format(layer.GetExtent()))
+
+#       Getting features
+
+# feature = layer.GetFeature(0)
+feature = layer.GetNextFeature()
+while feature:
+    # do something here
+    feature = layer.GetNextFeature()
+    layer.ResetReading() #need if looping again
+
+#       Getting feature’s attributes
+
